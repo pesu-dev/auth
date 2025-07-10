edf2742b8562b727e0232ca76748b8b1dca96a61
aditeyabaral <aditeya.baral@gmail.com>
2025-07-10 01:29:21 -0400

    Remove unused code

--- app/util.py
+++ app/util.py
@@ -2,8 +2,6 @@ import re
 import logging
 import gh_md_to_html
 
-from app.models import RequestModel
-
 
 def convert_readme_to_html():
     """
@@ -18,26 +16,3 @@ def convert_readme_to_html():
     with open("README.html", "w") as f:
         f.write(html)
     logging.info("README.md converted to HTML successfully.")
-
-
-def validate_input(data: dict) -> RequestModel:
-    """
-    Validate the input provided by the user
-
-    Args:
-        data (dict): The input data to validate. Should contain:
-            - username: str - The user's SRN, PRN, email, or phone number
-            - password: str - The user's password
-            - profile: bool (optional) - Whether to fetch user profile
-            - fields: List[str] (optional) - Profile fields to return
-
-    Returns:
-        RequestModel: The validated input data as a RequestModel instance.
-    """
-    logging.info(
-        f"Validating input: {data.get('username')=}, password={'*****' if data.get('password') else None}, "
-        f"profile={data.get('profile')}, fields={data.get('fields')}"
-    )
-    validated_data = RequestModel.model_validate(data)
-    logging.info("Input validation successful. All parameters are valid.")
-    return validated_data
