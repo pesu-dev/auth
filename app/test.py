14818cef5e13c9d5fb210f717ec5a324e0298420
abaksy <abaksy@gmail.com>
2025-07-13 11:29:03 -0700

    Cleanup

--- app/test.py
+++
@@ -1,9 +0,0 @@
-class CustomException(Exception):
-    def __init__(self, *args: object) -> None:
-        super().__init__(*args)
-
-
-try:
-    raise Exception("HAHAHAHA YOU SUCK")
-except CustomException as e:
-    print("Got this exception", e)
