689355d93ba8279eddc1d1bc71ca7f767522792d
Suraj B M <silicoflare@gmail.com>
2025-07-15 21:32:27 +0530

    Enhance README conversion to HTML by adding options for image downloading control in `convert_readme_to_html` function.

--- app/util.py
+++ app/util.py
@@ -12,7 +12,11 @@ def convert_readme_to_html():
     readme_content = re.sub(r":\w+: ", "", readme_content)
     with open("README_tmp.md", "w") as f:
         f.write(readme_content)
-    html = gh_md_to_html.main("README_tmp.md").strip()
+    html = gh_md_to_html.main(
+        "README_tmp.md",
+        enable_image_downloading=False,
+        image_paths=None,
+    ).strip()
     with open("README.html", "w") as f:
         f.write(html)
     logging.info("README.md converted to HTML successfully.")
